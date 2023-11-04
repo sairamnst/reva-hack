82e39f557d2f1f7418d1607ddf67f7431cb240c1
vijayarun_ <64945040+vijayarun00100@users.noreply.github.com>
2023-11-04 07:11:05 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route,Routes } from 'react-router-dom';
+
+import Main from './Main';
+import Coursepage from './Coursepage';
+import Record_server from './Record_server';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<Main />} />
+        <Route path="Main" element={<Main />} />
+        <Route path="courses" element={<Coursepage />} />
+        <Route path="Tasks" element={<Record_server />} />
+      </Routes>
+    </Router>
   );
 }
 
